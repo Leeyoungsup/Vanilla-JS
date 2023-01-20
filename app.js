2342df32e9919e5c0302361ad6cdb3aa4005660c
Leeyoungsup <liive29252@gmail.com>
2023-01-20 14:22:55 +0900

    CSS_in_Javascript_part_two

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
-console.dir(h1);
 
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const clickedClass = "clicked sexy-font";
+  if (h1.className == clickedClass) {
+    h1.className = "";
+  } else {
+    h1.className = clickedClass;
+  }
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOOOD");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
