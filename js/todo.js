0b07e7dc9eb8076285349fc132fddc13b9501030
Leeyoungsup <liive29252@gmail.com>
2023-01-25 18:03:15 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  console.log(span);
+  toDoList.appendChild(li);
+}
+
 function handleToDoSummit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newToDo, toDoInput.value);
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSummit);
